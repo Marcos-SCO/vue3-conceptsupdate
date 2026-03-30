23a2edeada29df381cc20265a30705282950d34e
marcos-sco <marcos_sco@outlook.com>
2026-03-30 14:34:52 -0300

    feat: add animation to loading state in SkeletonBookingItem and SkeletonCourseItem components

--- vue-courses/src/components/SkeletonBookingItem.vue
+++ vue-courses/src/components/SkeletonBookingItem.vue
@@ -4,7 +4,7 @@ import RoundedCard from '@/components/RoundedCard.vue';
 
 <template>
   <RoundedCard>
-    <div class="p-4 h-18 flex items-center justify-between">
+    <div class="p-4 h-18 flex items-center justify-between animate-pulse">
       <div class="bg-gray-300 rounded-full h-8 w-36"></div>
       <div class="bg-gray-300 rounded-full h-6 w-18"></div>
     </div>
--- vue-courses/src/components/SkeletonCourseItem.vue
+++ vue-courses/src/components/SkeletonCourseItem.vue
@@ -4,7 +4,7 @@ import RoundedCard from '@/components/RoundedCard.vue';
 
 <template>
   <RoundedCard>
-     <div class="flex flex-col border border-gray-200 justify-between">
+     <div class="flex flex-col border border-gray-200 justify-between animate-pulse">
         <div class="h-36 bg-gray-300">
 
         </div>
